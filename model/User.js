0cc0ad3e3d153f414d152c2462aeba96748ff320
Karine Marques Hara <karine.mhara@gmail.com>
2023-12-01 17:11:42 -0300

    feat: implement CRUD functions for User

---
+++ model/User.js
@@ -0,0 +1,61 @@
+const {UserModel} = require('./bd');
+
+module.exports = {
+    create: async (name, email, password, admin=false) => {
+
+        const user = await UserModel.create({
+            name,
+            email,
+            password,
+            admin,
+        });
+        return user;
+    },
+
+    getAll: async () => {
+        const users = await UserModel.findAll();
+        return users;
+    },
+
+    getById: async (id) => {
+        const user = await UserModel.findByPk(id);
+        return user;
+    },
+
+    getByEmail: async (email) => {
+        const user = await UserModel.findOne({
+            where: {
+                email,
+            },
+        });
+        return user;
+    },
+
+    isAdmin: async (id) => {
+        const user = await UserModel.findByPk(id);
+        return user.admin;
+    },
+
+    update: async (id, name, email, password, admin) => {
+        const user = await UserModel.update({
+            name,
+            email,
+            password,
+            admin,
+        }, {
+            where: {
+                id,
+            },
+        });
+        return user;
+    },
+
+    delete: async (id) => {
+        const user = await UserModel.destroy({
+            where: {
+                id,
+            },
+        });
+        return user;
+    },
+}
