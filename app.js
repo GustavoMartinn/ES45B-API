f3052b8c684e9438ab62242a1d4e28faac731679
Karine Marques Hara <karine.mhara@gmail.com>
2023-12-01 17:15:13 -0300

    feat: add /install route to create database tables

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
+const installRouter = require('./routes/install');
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(installRouter);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
---
+++ routes/install.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../model/User');
+
+const {sequelize} = require('../model/bd');
+
+router.get('/install', async (req, res) => {
+    console.log('Creating database...');
+    try {
+        await sequelize.sync({force:true});
+        res.send('Tables created');
+    } catch (error) {
+        console.log(error);
+        res.send('Error creating tables');
+    }  
+});
+
+module.exports = router;
