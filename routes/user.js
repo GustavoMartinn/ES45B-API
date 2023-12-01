d96c963094a1a55efc5b6b71100bddfdea528d00
Karine Marques Hara <karine.mhara@gmail.com>
2023-12-01 17:34:58 -0300

    feat: add update route at /user

--- routes/user.js
+++ routes/user.js
@@ -5,26 +5,26 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 
 let authorization = (req, res, next) => {
-    let token = req.headers["authorization"];
-    if (token == undefined) {
-      res.status(401).json({ status: "No token" });
-    }
-    token = token.replace("Bearer ", "");
-    console.log(token);
-    if (token) {
-      jwt.verify(token, "secret1234!@#$", (err, decoded) => {
-        if (err) {
-          res.status(401).json({ status: "Invalid token" });
-        } else {
-          console.log(decoded);
-          req.decoded = decoded;
-          next();
-        }
-      });
-    } else {
-      res.status(401).json({ status: "No token" });
-    }
-  };
+  let token = req.headers["authorization"];
+  if (token == undefined) {
+    res.status(401).json({ status: "No token" });
+  }
+  token = token.replace("Bearer ", "");
+  console.log(token);
+  if (token) {
+    jwt.verify(token, "secret1234!@#$", (err, decoded) => {
+      if (err) {
+        res.status(401).json({ status: "Invalid token" });
+      } else {
+        console.log(decoded);
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ status: "No token" });
+  }
+};
 
 router.post("/", async (req, res) => {
   let { name, email, password } = req.body;
@@ -38,24 +38,42 @@ router.post("/", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-    let { email, password } = req.body;
-    console.log(email);
-    console.log(password);
-    const user = await User.getByEmail(email);
-    if (user) {
-      if (user.password === password) {
-        const token = jwt.sign(
-          { id: user.id, email: user.email, admin: user.admin },
-          "secret1234!@#$",
-          { expiresIn: "1h" }
-        );
-        res.status(200).json({ logged: true, token });
-      } else {
-        res.status(401).json({ status: "Invalid password" });
-      }
+  let { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+  const user = await User.getByEmail(email);
+  if (user) {
+    if (user.password === password) {
+      const token = jwt.sign(
+        { id: user.id, email: user.email, admin: user.admin },
+        "secret1234!@#$",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ logged: true, token });
     } else {
-      res.status(401).json({ status: "Invalid email" });
+      res.status(401).json({ status: "Invalid password" });
     }
-  });
+  } else {
+    res.status(401).json({ status: "Invalid email" });
+  }
+});
+
+router.put("/update", authorization, async (req, res) => {
+  let { id } = req.decoded;
+  let { name, email, password } = req.body;
+  try {
+    let updated = await User.update(id, name, email, password);
+
+    if (updated) {
+      res.status(200).json({ status: "User updated" });
+    }
+    res.status(500).json({ status: "User not updated" });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ status: "Data invalid" });
+  }
+});
+
+
 
 module.exports = router;
