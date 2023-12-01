eae748cf1fde3e0852fddcc7aed937d0e3b07bd1
Karine Marques Hara <karine.mhara@gmail.com>
2023-12-01 17:52:28 -0300

    feat: add create route at /adm

--- routes/adm.js
+++ routes/adm.js
@@ -6,28 +6,38 @@ const User = require("../model/User");
 const { sequelize } = require("../model/bd");
 
 let authorizationAdm = (req, res, next) => {
-    let token = req.headers["authorization"];
-    if (token == undefined) {
-      res.status(401).json({ status: "No token" });
-    }
-    token = token.replace("Bearer ", "");
-    console.log(token);
-    if (token) {
-      jwt.verify(token, "secret1234!@#$", (err, decoded) => {
-        if (err) {
-          res.status(401).json({ status: "Invalid token" });
-        } else {
-          console.log(decoded);
-          req.decoded = decoded;
-          if (decoded.admin) {
-            next();
-          }
+  let token = req.headers["authorization"];
+  if (token == undefined) {
+    res.status(401).json({ status: "No token" });
+  }
+  token = token.replace("Bearer ", "");
+  console.log(token);
+  if (token) {
+    jwt.verify(token, "secret1234!@#$", (err, decoded) => {
+      if (err) {
+        res.status(401).json({ status: "Invalid token" });
+      } else {
+        console.log(decoded);
+        req.decoded = decoded;
+        if (decoded.admin) {
+          next();
         }
-      });
-    } else {
-      res.status(401).json({ status: "No token" });
-    }
-  };
+      }
+    });
+  } else {
+    res.status(401).json({ status: "No token" });
+  }
+};
 
+router.post("/create", authorizationAdm, async (req, res) => {
+  let { name, email, password, admin } = req.body;
+  try {
+    let user = await User.create(name, email, password, admin);
+    res.status(200).json({ status: "User created" });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ status: "Data invalid" });
+  }
+});
 
 module.exports = router;
